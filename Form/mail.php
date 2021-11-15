10716c9f59239c8b725cbd0ce7652869a2e00e9a
shirishakale <73546626+shirishakale@users.noreply.github.com>
2021-11-15 12:47:16 +0530

    Add files via upload

---
+++ Form/mail.php
@@ -0,0 +1,19 @@
+<?php
+//get data from form
+$name = $_POST['name'];
+$email = $_POST['emailaddress'];
+$message = $_POST['message'];
+$Number = $_POST['number'];
+
+$to = "shirishakale2525@gmail.com";
+
+$subject = "Mail from Website";
+$txt = "Name = " . $name . "\r\n Email = " . $email . "\r\n Message = " . $message . "\r\n Mobile Number = " . $Number;
+$headers = "From: noreply@yoursite.com" . "\r\n" . 
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    MAIL($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location: Thankyou.html");
+?>
