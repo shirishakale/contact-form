a2fc54247ddf4123e910103035d11f53206c7fab
shirishakale <73546626+shirishakale@users.noreply.github.com>
2021-11-15 13:12:46 +0530

    Update mail.php

--- Form/mail.php
+++ Form/mail.php
@@ -3,12 +3,12 @@
 $name = $_POST['name'];
 $email = $_POST['emailaddress'];
 $message = $_POST['message'];
-$Number = $_POST['number'];
+$number = $_POST['number'];
 
 $to = "shirishakale2525@gmail.com";
 
 $subject = "Mail from Website";
-$txt = "Name = " . $name . "\r\n Email = " . $email . "\r\n Message = " . $message . "\r\n Mobile Number = " . $Number;
+$txt = "Name = " . $name . "\r\n Email = " . $email . "\r\n Message = " . $message . "\r\n Mobile Number = " . $number;
 $headers = "From: noreply@yoursite.com" . "\r\n" . 
 "CC: somebodyelse@example.com";
 if($email!=NULL){
